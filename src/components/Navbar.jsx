329c6bb87c2739c103dc87ae4323706eed90c054
Mustafa Zorlu <mzorlu3579@gmail.com>
2024-09-29 20:27:48 +0300

    dark/light mode

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BsLightbulb } from "react-icons/bs";
 import { AiOutlineShoppingCart } from "react-icons/ai";
+import { MdOutlineFlashlightOff } from "react-icons/md";
+import { MdOutlineFlashlightOn } from "react-icons/md";
 
 const Navbar = () => {
+    const [color, setColor] = useState(false);
+
+    useEffect(() => {
+        const root = document.getElementById("root");
+        if (color) {
+            root.style.backgroundColor = "#111";
+            root.style.color = "#f7f7f7";
+        } else {
+            root.style.backgroundColor = "#f7f7f7";
+            root.style.color = "#111";
+        }
+    });
     return (
-        <div>
+        <div className="flex items-center justify-between px-3 h-20">
             <div>Trenyol</div>
-            <div>
+            <div className="flex items-center space-x-4 ">
                 <input type="text" placeholder="Search" />
-                <BsLightbulb />
+                <div onClick={() => setColor(!color)}>
+                    {!color ? (
+                        <MdOutlineFlashlightOn size={24} />
+                    ) : (
+                        <MdOutlineFlashlightOff size={24} />
+                    )}
+                </div>
                 <div className="relative">
-                    <AiOutlineShoppingCart />
-                    <span>3</span>
+                    <AiOutlineShoppingCart size={24} />
+                    <span className="absolute right-0 top-0 px-1 bg-red-500 text-white rounded-full text-sm p-0.5">
+                        3
+                    </span>
                 </div>
             </div>
         </div>
--- src/containers/PageContainer.jsx
+++ src/containers/PageContainer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const PageContainer = ({ children }) => {
-    return <div className="w-3/4 m-auto bg-gray-100">{children}</div>;
+    return <div className="w-3/4 m-auto">{children}</div>;
 };
 
 export default PageContainer;
